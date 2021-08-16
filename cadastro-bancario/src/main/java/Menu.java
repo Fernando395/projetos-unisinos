8849e1f943c4dadbe9cb86e3edeca4e98dc3773b
Fernando395 <Fe395rodrigues@gmail.com>
2021-08-16 16:39:24 -0300

    Criado arquivo README.md

--- cadastro-bancario/src/main/java/Menu.java
+++ cadastro-bancario/src/main/java/Menu.java
@@ -1,6 +1,6 @@
 public class Menu {
     public void greetings() {
-        System.out.println("Bem vindo ao Banco do Rio Grande.");
+        System.out.println("Bem vindo ao Banco Rio Grande.");
         System.out.println("Para criar uma conta Preencha os dados a baixo.");
     }
 }
