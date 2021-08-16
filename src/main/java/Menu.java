e1a17523d8cf1b7783a4a79efe75e18789eac86f
Fernando395 <Fe395rodrigues@gmail.com>
2021-08-16 16:44:21 -0300

    Criado arquivo README.md

--- src/main/java/Menu.java
+++
@@ -1,7 +0,0 @@
-public class Menu {
-    public void greetings() {
-        System.out.println("Bem vindo ao Banco do Rio Grande.");
-        System.out.println("Para criar uma conta Preencha os dados a baixo.");
-    }
-}
-
